fee9f0cb587cffaad29121202e28d90477cc1f3c
Gustavo Magalhães Do Nascimento <53844614893@info.local>
2023-06-01 17:18:19 -0300

    Conteudo 01/06

---
+++ Projeto Gamer Full/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Gamer_Full.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
